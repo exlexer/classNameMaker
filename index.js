a1cc4e160c117fea539b046c95474285ca91c5bf
Kyle Hilton <kylehilton@Kyles-MacBook-Pro.local>
2020-01-27 01:15:09 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+module.exports = (block, className) => (
+  element,
+  modifiers,
+  ...additionalClasses
+) => {
+  const _element = element ? `__${element}` : '';
+  const _classNames = className.split(' ').filter(value => value);
+  const _modifiers = Object.keys(modifiers);
+
+  return [
+    `${block}${_element}`,
+    ..._modifiers.reduce(
+      (acc, _modifier) => {
+        const value = modifiers[_modifier];
+    
+        if (value) {
+          acc.push(
+            `${block}${_element}--${_modifier}`,
+            ..._classNames.map(_className => `${_className}${_element}--${_modifier}`)
+          );
+        }
+    
+        return acc;
+      },
+      [],
+    ),
+    ..._classNames.map(_className => `${_className}${_element}`),
+    ...additionalClasses
+    ].join(' ');
+};
---
+++ test.js
@@ -0,0 +1,40 @@
+const assert = require('assert');
+
+const classMaker = require('./index');
+
+const getClasses = () => {
+  const _getClasses = classMaker('default_block', 'given_block');
+  return _getClasses('section', { modified: true, 'not_modified': false }, 'added');
+}
+
+it('should create default classes', () => {
+  const classes = getClasses();
+
+  assert.ok(classes.includes('default_block__section'));
+});
+
+it('should create given classes', () => {
+  const classes = getClasses();
+
+  assert.ok(classes.includes('given_block__section'));
+});
+
+it('should add truthy modifiers', () => {
+  const classes = getClasses();
+
+  assert.ok(classes.includes('default_block__section--modified'));
+  assert.ok(classes.includes('given_block__section--modified'));
+});
+
+it('should not add falsy modifiers', () => {
+  const classes = getClasses();
+
+  assert.equal(classes.includes('default_block__section--not-modified'), false);
+  assert.equal(classes.includes('given_block__section--not-modified'), false);
+});
+
+it('should add added classes default classes', () => {
+  const classes = getClasses();
+
+  assert.ok(classes.includes('added'));
+});
